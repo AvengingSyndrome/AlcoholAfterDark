f4e8fe0a2f6633e8cf4a4ebeef4699a5ab2f3322
Andrew <AvenginSyndrome@users.noreply.github.com>
2015-04-18 16:40:23 +1000

    Comments getting inserted and displayed correctly

--- aah/Handler/Offer.hs
+++ aah/Handler/Offer.hs
@@ -8,7 +8,7 @@ getOfferR offerId = do
     offer <- runDB $ get404 offerId
     proposal <- runDB $ get404 $ offerProposal offer
     let sale = proposal
-    comments <- runDB $ selectList [CommentOffer ==. offerId] [Desc CommentPostTime]
+    comments <- runDB $ selectList [CommentOffer ==. offerId] [Asc CommentPostTime]
     authid <- maybeAuthId 
     case authid of 
         Just aid -> do
@@ -19,7 +19,7 @@ getOfferR offerId = do
              $(widgetFile "offer")
              $(widgetFile "comments")
              if (aid == offerUser offer || aid == proposalPosterUser proposal) 
-		then $(widgetFile "commentform")
+                then $(widgetFile "commentform")
                 else mempty
              
         Nothing -> 
@@ -28,13 +28,23 @@ getOfferR offerId = do
              $(widgetFile "sale")
              $(widgetFile "comments")
 postOfferR :: OfferId -> Handler Html
-postOfferR offerId = error "Not yet implemented: postOfferR"
-
---Comment
---    postTime UTCTime
---    user UserId
---    offer OfferId
---    description Html
+postOfferR offerId = do
+    aid <- requireAuthId
+    offer <- runDB $ get404 offerId
+    proposal <- runDB $ get404 $ offerProposal offer
+    if (aid == offerUser offer || aid == proposalPosterUser proposal) 
+      then do
+       ((res,_),_) <- runFormPost $  makeCommentForm aid offerId
+       case res of 
+         FormSuccess comment -> do
+            _ <- runDB $ insert comment   
+            setMessage $ "Posted!"
+            getOfferR offerId
+         _ ->  do 
+            getOfferR offerId
+      else do 
+       setMessage "You can't post to this page!"
+       getOfferR offerId
 
 makeCommentForm :: Key User -> Key Offer -> Form Comment
 makeCommentForm uid offer = renderDivs $ Comment
